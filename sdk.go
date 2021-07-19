af7b641e60b11b3fa11f633a3209e01d76d8b825
ricl <riclavaz@gmail.com>
2021-07-19 17:37:57 +0800

    send request to ccs endpoint

--- sdk.go
+++ sdk.go
@@ -1,16 +1,22 @@
 package ccssdk
 
 import (
+	context "context"
+	"fmt"
 	"log"
 	"strings"
+	"time"
+
+	grpc "google.golang.org/grpc"
 )
 
 type CcsSdk struct {
+	CcsEndpoint         string
 	HandleFuncs         HandleFuncs
 	messageQueueService *MessageQueueService
 }
 
-func InitCcsSdk(handleFuncs HandleFuncs) (*CcsSdk, error) {
+func InitCcsSdk(ccs_endpoint string, handleFuncs HandleFuncs) (*CcsSdk, error) {
 
 	var fixedHandleFuncs HandleFuncs
 	fixedHandleFuncs = append(fixedHandleFuncs, NewDefaultHandle())
@@ -27,6 +33,7 @@ func InitCcsSdk(handleFuncs HandleFuncs) (*CcsSdk, error) {
 	}
 
 	sdk := &CcsSdk{
+		CcsEndpoint:         ccs_endpoint,
 		HandleFuncs:         fixedHandleFuncs,
 		messageQueueService: NewMessageQueueService(messageQueueRepo),
 	}
@@ -35,4 +42,36 @@ func InitCcsSdk(handleFuncs HandleFuncs) (*CcsSdk, error) {
 	sdk.messageQueueService.Subscribe(mqRespTopic, sdk.HandleFuncs)
 
 	return sdk, nil
+}
+
+func (s *CcsSdk) Send(msg string, timeout int) error {
+	// TODO optimize me, connection multiplexing
+	conn, err := grpc.Dial(s.CcsEndpoint, grpc.WithInsecure())
+
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	messageClient := NewServiceMessageClient(conn)
+
+	// TODO from env
+	secretKey := "__SECRET_KEY__"
+
+	// TODO fill AppId, ServiceId etc.
+	request := &MessageRequest{
+		AppId:     "__APP_ID__",
+		ServiceId: "__SERVICE_ID__",
+		Payload:   msg,
+		Timestamp: time.Now().Unix(),
+	}
+
+	request.Token = HmacSha256Base64(fmt.Sprintf("%s%d", request.AppId, request.Timestamp), secretKey)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(timeout))
+	defer cancel()
+
+	_, err = messageClient.OnMessageRequest(ctx, request)
+	return err
 }
